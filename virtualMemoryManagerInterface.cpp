bada2210d3c9ebb8c22aa42619932fd87942b18a
TylerBohrer <tjb5704@psu.edu>
2017-04-26 15:19:59 -0400

    Got it working with inputs! Still needs some work done but we are getting clos to the end and just testbenching

--- virtualMemoryManagerInterface.cpp
+++ virtualMemoryManagerInterface.cpp
@@ -10,31 +10,32 @@ int min = 0;
 int counter = -1;
 class memoryManager : public virtualMemoryManagerInterface {
 	public:
-	memoryManager(ReplacementPolicy p, unsigned int pS, unsigned int nF, unsigned int vA)
-		: virtualMemoryManagerInterface(p, pS, nF, vA) {
-	}
-	ReplacementPolicy type = virtualMemoryManagerInterface::policy; // policy
-	unsigned int framesAmount = virtualMemoryManagerInterface::numFrames; // num of frames
-	unsigned int n = virtualMemoryManagerInterface::N; // N = page address bit size... 2^ N = The physical frame / virtual page size to manage(bytes) 
-	unsigned int virt = virtualMemoryManagerInterface::virtualAddressSpaceSize; // 2^virtualAddressSpaceSize = total virtual address space(bytes)
-	unsigned long long worker(unsigned long long);
-	virtual unsigned long long memoryAccess(unsigned long long address) 
-	{
-		unsigned long long place = worker(address);
-		return place;
-	}
+		memoryManager(ReplacementPolicy p, unsigned int pS, unsigned int nF, unsigned int vA)
+			: virtualMemoryManagerInterface(p, pS, nF, vA) {
+		}
+		ReplacementPolicy type = virtualMemoryManagerInterface::policy; // policy
+		unsigned int framesAmount = virtualMemoryManagerInterface::numFrames; // num of frames
+		unsigned int n = virtualMemoryManagerInterface::N; // N = page address bit size... 2^ N = The physical frame / virtual page size to manage(bytes) 
+		unsigned int virt = virtualMemoryManagerInterface::virtualAddressSpaceSize; // 2^virtualAddressSpaceSize = total virtual address space(bytes)
+		unsigned long long worker(unsigned long long);
+		virtual unsigned long long memoryAccess(unsigned long long address) 
+		{
+			unsigned long long place = worker(address);
+			return place;
+		}
 
 
 
 };
+int remaining = 0;
 unsigned long long memoryManager::worker(unsigned long long mem)
 {
 	counter += 1;
 	int space = framesAmount; // max amount of frames that can be stored
 	int v_space = pow(2, virt);
-	int remaining = space;
+	//static int remaining = space;
 	int * pos; // used in FIFO
-	int * recent; // used in LRU
+	static int * recent; // used in LRU
 	unsigned long long * taken;//int p_space = framesAmount *pow(2, n); // the total size of the physical memory space
 	pos = new int[space];
 	taken = new unsigned long long[space];
@@ -54,7 +55,7 @@ unsigned long long memoryManager::worker(unsigned long long mem)
 
 		}
 		else { // will need to update this to be the mapping between vm and pm... rn have virtual memory lru and such will need to convert to physical memory and such
-			if (remaining == 0)
+			if (remaining == space)
 			{//need to swap
 			 // need to get the lru
 				int y = 0;
@@ -78,7 +79,7 @@ unsigned long long memoryManager::worker(unsigned long long mem)
 			{//dont need to swap but remaining must decrement by one
 				taken[space - remaining] = mem;
 				recent[space - remaining] = counter;
-				remaining -= 1;
+				remaining += 1;
 				return space - remaining + 1;
 
 			}
@@ -96,12 +97,14 @@ unsigned long long memoryManager::worker(unsigned long long mem)
 		}
 		if (found)
 		{// dont need to update pos since its only updated when it is inserted
+			cout << "it was found" << endl;
 			return x;
 		}
 		else {
-			if (remaining == 0)
+			if (remaining == space)
 			{//need to swap
 			 //find the min in
+				cout << "it was swapped" << endl;
 				if (min > space - 1) { min = 0; } // min has done a full recycle so must be reset now
 				int spot = min;
 				taken[min] = 0;
@@ -113,9 +116,11 @@ unsigned long long memoryManager::worker(unsigned long long mem)
 			}
 			else
 			{//dont need to swap but remaining must decrement by one
+				cout << "There was room" << endl;
+				cout << space << " : " << remaining << endl;
 				taken[space - remaining] = mem;
 				pos[space - remaining] = counter;
-				remaining -= 1;
+				remaining += 1;
 				return space - remaining + 1;
 			}
 		}
@@ -127,7 +132,7 @@ unsigned long long memoryManager::worker(unsigned long long mem)
 
 
 }
-/*oiganally used moving on to another way to implement it
+/*oiganally used moving on to another way to implement it... will take this completely out once getting code to run better
 unsigned long long virtualMemoryManagerInterface::memoryAccess(unsigned long long mem)
 {
 	counter += 1;
@@ -241,10 +246,22 @@ unsigned long long virtualMemoryManagerInterface::memoryAccess(unsigned long lon
 int main() 
 {// fixed the errors with 
 	virtualMemoryManagerInterface * memManager = new memoryManager(FIFO, 8, 5, 16);
+	unsigned long long help1 = 10;
+	unsigned long long help2 = 110;
+	unsigned long long help3 = 1110;
+	unsigned long long help4 = 11110;
+	unsigned long long help5 = 111110;
+	unsigned long long help6 = 1111110;
+	cout<<memManager->memoryAccess(help1)<<endl;
+	cout<<memManager->memoryAccess(help2) << endl;
+	cout << memManager->memoryAccess(help3) << endl;
+	cout << memManager->memoryAccess(help4) << endl;
+	cout << memManager->memoryAccess(help5) << endl;
+	cout << memManager->memoryAccess(help6) << endl;
+	cout << memManager->numberPageSwaps() << endl;
+
 	delete memManager;
-	int tesst = 0;
-	cout << "this is some words" << endl;
-	cout << tesst << endl;
+	int tesst = 0; // for some reason window closes automatic after it has done run through so I added this to stop it will look into removing
 	cin >> tesst;
 
 	return 0;
