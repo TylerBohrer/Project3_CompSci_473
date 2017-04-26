31a6d8ea3805aca670fc2c4a8e7a6e7010c3fb77
Sam Barnes <mistersamuelbarnes@gmail.com>
2017-04-26 16:07:02 -0400

    Now manages physical memory
    memoryAccess() returns the physical address that corresponds to logical address mem

--- memoryManager.cpp
+++ memoryManager.cpp
@@ -2,101 +2,91 @@
 using namespace std;
 // need to get the info for mem
 
-int min = 0;
+int currentFifoFrame = 0;
 int counter = -1;
 
 unsigned long long memoryManager::memoryAccess(unsigned long long mem) {
 	counter++;
 
-	int space = numFrames; // max amount of frames that can be stored
-	//int v_space = pow(2, virt);
-	int remaining = space;
+	// find the page corresponding to logical address mem
+	unsigned int pageNumber = numPages;
+	for (unsigned int i = 0; i < numPages; i++) {
+		if (mem <= virtualPages[i].endAddr) {
+			pageNumber = i;
+			break; // breaks when logical address mem is within page's range
+		}
+	}
+	if (pageNumber == numPages) { // no page contains this address
+		cout << "Invalid logical memory address\n";
+		return -1;
+	}
 
-	int* pos; // used in FIFO
-	pos = new int[space];
-	int* recent; // used in LRU
+	// used to convert from virtual to physical address
+	int memOffsetFromPageStart = mem - virtualPages[pageNumber].startAddr;
+
 	unsigned long long* taken;
-	//int p_space = framesAmount *pow(2, n); // the total size of the physical memory space
-	taken = new unsigned long long[space];
-	recent = new int[space]; // set the size to the max number of elements that can be used for this
-	// first need to set up way to keep track what is in the memory
+	taken = new unsigned long long[numFrames];
 
-	// Second need a way to swap pos with elements depending on the replacement policy
+	/* second need a way to swap pos with elements depending on the replacement policy */
 	if (policy == LRU) {
-		//check if it is init
-		bool found = false;
-		int x = 0;
-		for (x = 0; x < space - remaining; x++) {
-			if (taken[x] == mem) {
-				break;
+		//check if logical address mem is already in physical memory
+		for (unsigned int frameNumber = 0; frameNumber < numFrames - freeFrames; frameNumber++) {
+			if (physicalFrames[frameNumber].startAddr <= mem && mem <= physicalFrames[frameNumber].endAddr) { // found
+				recent[frameNumber] = counter;
+				return physicalFrames[frameNumber].startAddr + memOffsetFromPageStart;
 			}
 		}
-		if (found) {
-			recent[x] = counter;
-			return x;
-		}
-		else {
-			// will need to update this to be the mapping between vm and pm...
-			// rn have virtual memory lru and such will need to convert to physical memory and such
-			if (remaining == 0) { // need to swap
-				// need to get the lru
-				int y = 0;
-				int smallest = counter;
-				int spos = -1;
-				for (y = 0; y < space; y++) {
-					if (recent[y] < smallest) {
-						smallest = recent[y];
-						spos = y;
-					}
-				} // have smallest and its pos now
-				taken[spos] = mem;
-				recent[spos] = counter;
-				swap(mem, mem);
-				return spos;
-			}
-			else { // dont need to swap but remaining must decrement by one
-				taken[space - remaining] = mem;
-				recent[space - remaining] = counter;
-				remaining--;
-				return space - remaining + 1;
+		// not found in physical memory
 
-			}
+		if (freeFrames == 0) { // need to swap
+			// need to get the lru
+			unsigned int smallest = counter;
+			unsigned int lruFrameNumber = -1;
+			for (unsigned int y = 0; y < numFrames; y++) {
+				if (recent[y] < smallest) {
+					smallest = recent[y];
+					lruFrameNumber = y;
+				}
+			} // have lru frame and its frame number now
+			physicalFrames[lruFrameNumber] = virtualPages[pageNumber];
+			recent[lruFrameNumber] = counter;
+			swap(lruFrameNumber, pageNumber);
+			return physicalFrames[lruFrameNumber].startAddr + memOffsetFromPageStart;
 		}
+
+		// dont need to swap but remaining must decrement by one
+		physicalFrames[numFrames - freeFrames] = virtualPages[pageNumber];
+		recent[numFrames - freeFrames] = counter;
+		freeFrames--;
+		return physicalFrames[numFrames - (freeFrames + 1)].startAddr + memOffsetFromPageStart;
 
 	}
 	else if (policy == FIFO) {
-		//check if it is init
-		bool found = false;
-		int x = 0;
-		for (x = 0; x < space - remaining; x++) {
-			if (taken[x] == mem) {
-				break;
+		//check if logical address mem is already in physical memory
+		for (unsigned int frameNumber = 0; frameNumber < numFrames - freeFrames; frameNumber++) {
+			if (physicalFrames[frameNumber].startAddr <= mem && mem <= physicalFrames[frameNumber].endAddr) { // found
+				// dont need to update pos since its only updated when it is inserted
+				return physicalFrames[frameNumber].startAddr + memOffsetFromPageStart;
 			}
 		}
 
-		if (found) { // dont need to update pos since its only updated when it is inserted
-			return x;
+		if (freeFrames == 0) { // need to swap
+			// find the oldest frame index to swap with
+			if (currentFifoFrame == numFrames) {
+				currentFifoFrame = 0; // min has done a full recycle so must be reset now
+			}
+			physicalFrames[currentFifoFrame] = virtualPages[pageNumber];
+			pos[currentFifoFrame] = counter;
+			swap(currentFifoFrame, pageNumber);
+			currentFifoFrame++;
+			return physicalFrames[currentFifoFrame - 1].startAddr + memOffsetFromPageStart;
 		}
 
-		if (remaining == 0) { // need to swap
-			// find the min in
-			if (min > space - 1) {
-				min = 0; // min has done a full recycle so must be reset now
-			}
-			int spot = min;
-			taken[min] = 0;
-			min += 1;
-			taken[min - 1] = mem;
-			pos[min - 1] = counter;
-			swap(mem, mem);
-			return min - 1;
-		}
-		else { // dont need to swap but remaining must decrement by one
-			taken[space - remaining] = mem;
-			pos[space - remaining] = counter;
-			remaining -= 1;
-			return space - remaining + 1;
-		}
+		// dont need to swap but remaining must decrement by one
+		physicalFrames[numFrames - freeFrames] = virtualPages[pageNumber];
+		pos[numFrames - freeFrames] = counter;
+		freeFrames--;
+		return physicalFrames[numFrames - (freeFrames + 1)].startAddr + memOffsetFromPageStart;
 	}
 	else {
 		cout << "Invalid Policy\n";
@@ -107,9 +97,6 @@ unsigned long long memoryManager::memoryAccess(unsigned long long mem) {
 
 
 int main() {
-	// as of now am returning the virtual memory address need to return the phyiscal memory address
-	// lru and fifo are good so far
-
 	// just a sample object to make sure things are being constructed alright
 	virtualMemoryManagerInterface * memManager = new memoryManager(FIFO, 8, 5, 16);
 
--- memoryManager.hpp
+++ memoryManager.hpp
@@ -5,11 +5,41 @@
 #include "virtualMemoryManagerInterface.hpp"
 
 class memoryManager : public virtualMemoryManagerInterface {
+private:
+	unsigned long long pageSize;
+	unsigned long long physicalMemTotalSize;
+	unsigned long long virtualMemTotalSize;
+	unsigned int numPages;
+	struct Page {
+		// both inclusive
+		unsigned long long startAddr;
+		unsigned long long endAddr;
+	};
+	Page* virtualPages;
+	Page* physicalFrames;
+	int freeFrames;
+	unsigned int* recent; // used in LRU
+	unsigned int* pos; // used in FIFO
+
 public:
 	memoryManager(ReplacementPolicy p, unsigned int pS, unsigned int nF, unsigned int vA)
 		: virtualMemoryManagerInterface(p, pS, nF, vA) {
+		pageSize = pow(2, N);
+		physicalMemTotalSize = numFrames * pageSize;
+		virtualMemTotalSize = pow(2, virtualAddressSpaceSize);
+		numPages = virtualMemTotalSize / virtualAddressSpaceSize;
+
+		physicalFrames = new Page[numFrames];
+		virtualPages = new Page[numPages];
+		for (unsigned int i = 0; i < numPages; i++) {
+			virtualPages[i].startAddr = i * virtualAddressSpaceSize;
+			virtualPages[i].endAddr = (i + 1) * virtualAddressSpaceSize - 1;
+		}
+		
+		freeFrames = numFrames;
+		recent = new unsigned int[numFrames];
+		pos = new unsigned int[numFrames];
 	}
 
 	unsigned long long memoryAccess(unsigned long long address);
-
 };
